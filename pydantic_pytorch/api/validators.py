166d97873b84e79459390cad4e73d661b7630418
adnanhd <adnanharundogan@gmail.com>
2024-07-07 04:12:16 +0300

    Unfinished: update torch_device.py

--- pydantic_pytorch/api/validators.py
+++ pydantic_pytorch/api/validators.py
@@ -3,7 +3,7 @@ import dataclasses
 from typing import Any
 from .source_types import TORCH_DEVICE_TYPE, TORCH_DEVICE_INDEX
 from pydantic_core import core_schema, CoreSchema
-from pydantic import ValidatorFunctionWrapHandler, GetCoreSchemaHandler
+from pydantic import ValidatorFunctionWrapHandler, SerializerFunctionWrapHandler, GetCoreSchemaHandler
 
 
 @dataclasses.dataclass(slots=True)
@@ -26,10 +26,11 @@ class TorchDeviceValidator:
     def __get_pydantic_core_schema__(self, source_type: Any, handler: GetCoreSchemaHandler) -> CoreSchema:
         # TODO: no validation for device_type and device_index
         print('hello!!!')
-        import pdb; pdb.set_trace()
 
-        schema = core_schema.is_instance_schema(torch.device)
-        validators = [schema]
+        serialization = core_schema.wrap_serializer_function_ser_schema(serialize_torch_device)
+        schema = core_schema.is_instance_schema(torch.device, serialization=serialization)
+        configuration = core_schema.no_info_before_validator_function(validate_torch_device, schema=schema)
+        validators = [configuration, schema]
 
         if self.device_type is not None:
             validators.append(core_schema.no_info_wrap_validator_function(
@@ -40,3 +41,20 @@ class TorchDeviceValidator:
                 self.torch_device_index_validator, schema=schema))
 
         return core_schema.chain_schema(validators)
+
+
+def serialize_torch_device(x: torch.device, nxt: SerializerFunctionWrapHandler) -> str:
+    if x.index is not None:
+        return nxt(f'{x.type}:{x.index}')
+    return nxt(x.type)
+
+
+def validate_torch_device(x: Any, strict : bool = False) -> torch.device:
+    if isinstance(x, torch.device):
+        return x
+    elif strict:
+        raise ValueError(f'Invalid torch device: {x!r}')
+    try:
+        return torch.device(x)
+    except Exception as e:
+        raise ValueError(f'Invalid torch device: {x!r}') from e
--- torch_device.py
+++ torch_device.py
@@ -5,15 +5,38 @@ from pydantic._internal import _known_annotated_metadata, _typing_extra, _valida
 import dataclasses
 from pydantic_pytorch.annotations.multiarray.torch_device import TORCH_DEVICE_TYPE
 from typing_extensions import TypedDict, Literal, Required, Any
-from pydantic import NonNegativeInt, ConfigDict, validate_call, GetCoreSchemaHandler, ValidatorFunctionWrapHandler, SerializerFunctionWrapHandler
+from pydantic import NonNegativeInt, ConfigDict, validate_call, GetCoreSchemaHandler, ValidatorFunctionWrapHandler, SerializerFunctionWrapHandler, InstanceOf, PositiveInt
 from pydantic_core import core_schema, CoreSchema
 from typing import Iterable
 import torch
+from pydantic_pytorch.api.schema import TorchDeviceSchema
+from pydantic_pytorch.api.validators import TorchDeviceValidator
+
 
 def to_string(x: Any, nxt: SerializerFunctionWrapHandler) -> str:
     return nxt(str(x))
 
 
-int_validator = SchemaValidator({'type': 'int', 'ge': 0, 'serialization': 'int'}).validate_python
-int_serializer = SchemaSerializer({'type': 'str', 'serialization': {'type': 'function-wrap', 'function': to_string}}).to_python
-str_validator = SchemaValidator({'type': 'str', 'strict': False}).validate_python
+def serialize_torch_device(x: torch.device, nxt: SerializerFunctionWrapHandler) -> str:
+    print('serialize_torch_device', x)
+    if x.index is not None:
+        return nxt(f'{x.type}:{x.index}')
+    return nxt(x.type)
+
+
+int_validator_schema = {'type': 'int', 'ge': 0}
+int_serializer_schema = {'type': 'str', 'serialization': {'type': 'function-wrap', 'function': to_string}}
+
+int_validator = SchemaValidator(int_validator_schema)
+int_serializer = SchemaSerializer(int_serializer_schema)
+
+
+torch_device_validator_schema = core_schema.is_instance_schema(torch.device)
+torch_device_serializer_schema = {'type': 'str', 'serialization': {'type': 'function-wrap', 'function': serialize_torch_device}}
+
+torch_device_validator = SchemaValidator(torch_device_validator_schema)
+torch_device_serializer = SchemaSerializer(torch_device_serializer_schema)
+
+
+class MyModel(BaseModel):
+    a: TorchDeviceValidator(device_type='cuda')
