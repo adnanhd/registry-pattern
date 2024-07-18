747cf3a53098b74425e6fc28421f367a95d9ab8b
adnanhd <adnanharundogan@gmail.com>
2024-07-18 20:31:01 +0300

    untested: patterns/registry

--- tests/test_registry/test_class_registry.py
+++ tests/test_registry/test_class_registry.py
@@ -5,49 +5,38 @@ Test class registry.
 from typing import Protocol
 import pytest
 
-from pydantic_pytorch.patterns.registry import ClassRegistry, RegistryError
-
+from pydantic_pytorch.patterns.registry import make_class_registry, ClassRegistry, StructuringError, RegistryError
 
 class Foo(Protocol):
-    def foo(self, x: int, y: int) -> int:
+    def test_method(self, x: int, y: int) -> int:
         ...
 
-
-class FooClassRegistry(ClassRegistry[Foo]):
-    """Class registry for Foo."""
-
-
-def test_class_registry_with_non_subclass():
-    with pytest.raises(RegistryError) as exc_info:
-        @FooClassRegistry.register_class
-        class Bar:
-            def foo(self, x: int, y: int) -> int:
-                return 2
-    err_msg = exc_info.value.args[0]
-    assert err_msg == "<class 'test_class_registry.test_class_registry_with_non_subclass.<locals>.Bar'> is not of cls <class 'test_class_registry.FooClassRegistry'>"
+@pytest.fixture
+def Registry():
+    return make_class_registry('FooClass', protocol=Foo)
 
 
-
-def test_class_registry_with_subclass():
-    @FooClassRegistry.register_class
-    @FooClassRegistry.register
+def test_class_registry_with_subclass(Registry: ClassRegistry):
+    @Registry.register_class
+    @Registry.__class__.register
     class Bar:
-        def foo(self, x: int, y: int) -> int:
+        def test_method(self, x: int, y: int) -> int:
             return 2
 
-    assert FooClassRegistry.get_registered('Bar') is Bar
+    assert Registry.get_registry_item('Bar') is Bar
 
 
     with pytest.raises(RegistryError) as exc_info:
-        FooClassRegistry.register_class(Bar)
+        Registry.register_class(Bar)
 
-    assert exc_info.value.args[0] == "FooClassRegistry: 'Bar' is already registered"
+    assert exc_info.value.args[0] == "FooClassRegistry: 'Bar' already registered"
 
-def test_class_registry_with_invalid_class():
-    with pytest.raises(RegistryError) as exc_info:
-        @FooClassRegistry.register_class
-        @FooClassRegistry.register
+def test_class_registry_with_invalid_class(Registry: ClassRegistry):
+    with pytest.raises(StructuringError) as exc_info:
+        @Registry.register_class
+        @Registry.__class__.register
         class Baz:
-            ...
+            def test2_method(self, x: int, y: int) -> int:
+                return 2
 
     assert exc_info.value.args[0] == "<class 'test_class_registry.test_class_registry_with_invalid_class.<locals>.Baz'> is not of type <class 'test_class_registry.Foo'>"
