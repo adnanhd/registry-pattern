ac715708da9d79c1126ca0b742ff390e6fb7f104
adnanhd <adnanharundogan@gmail.com>
2024-08-07 21:31:11 +0300

    Add documentation and deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Install dependencies
+pip install pytest pytest-cov black flake8 pyright
+
+# Run tests
+pytest -vv --cov
+
+# Run type checking
+pyright
+
+# Check code format
+black --check .
+
+# Lint code
+flake8 .
